0f391c4b19e278643dad06b871396c2778fd7b90
ericlewis <ericlewis777@gmail.com>
2019-09-09 17:19:07 -0400

    notices have pictures

--- Delta/Stores/NotificationStore.swift
+++ Delta/Stores/NotificationStore.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import UserNotifications
+import MobileCoreServices
 
 extension UNUserNotificationCenter {
     func add(_ request: UNNotificationRequest) {
@@ -15,6 +16,17 @@ extension UNNotificationRequest {
         content.title = "Download Finished"
         content.body = "\(game.title ?? "No Title") is ready to play"
         content.userInfo = ["id": game.id!]
+        
+        let nakedRequest = UNNotificationRequest(identifier: id,
+                                                content: content,
+                                                trigger: nil)
+        
+        guard let url = game.image else { return nakedRequest }
+        guard let imageData = NSData(contentsOf: url) else { return nakedRequest }
+        guard let attachment = UNNotificationAttachment.create("image.png", data: imageData) else { return nakedRequest }
+        
+        content.attachments = [attachment]
+        
         let request = UNNotificationRequest(identifier: id,
                                             content: content,
                                             trigger: nil)
@@ -29,9 +41,33 @@ extension UNNotificationRequest {
         
         content.title = "Save Successful"
         content.body = "\(game.title ?? "No Title") - \(game.console.title)"
+        content.attachments = [try! UNNotificationAttachment(identifier: "image.png", url: imageURL, options: [UNNotificationAttachmentOptionsTypeHintKey: kUTTypePNG])]
         let request = UNNotificationRequest(identifier: id, content: content, trigger: nil)
         
         return request
+    }
+}
+
+// LIFTED FROM SO, probably need to change
+extension UNNotificationAttachment {
+
+    /// Save the image to disk
+    static func create(_ imageFileIdentifier: String, data: NSData) -> UNNotificationAttachment? {
+        let fileManager = FileManager.default
+        let tmpSubFolderName = ProcessInfo.processInfo.globallyUniqueString
+        let tmpSubFolderURL = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(tmpSubFolderName, isDirectory: true)
+
+        do {
+            try fileManager.createDirectory(at: tmpSubFolderURL!, withIntermediateDirectories: true, attributes: nil)
+            let fileURL = tmpSubFolderURL?.appendingPathComponent(imageFileIdentifier)
+            try data.write(to: fileURL!, options: [])
+            let imageAttachment = try UNNotificationAttachment(identifier: imageFileIdentifier, url: fileURL!, options: [UNNotificationAttachmentOptionsTypeHintKey: kUTTypeJPEG])
+            return imageAttachment
+        } catch let error {
+            print("error \(error)")
+        }
+
+        return nil
     }
 }
 
